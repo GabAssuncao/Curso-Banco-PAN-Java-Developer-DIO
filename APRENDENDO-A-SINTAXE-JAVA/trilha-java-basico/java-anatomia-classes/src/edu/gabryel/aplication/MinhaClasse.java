1955d9fc54b24f637df14a4b6adf217ae7c915fd
GabAssuncao <byeloliveira@gmail.com>
2023-03-19 13:13:56 -0300

    Inclusao da aula anatomia das classes

---
+++ APRENDENDO-A-SINTAXE-JAVA/trilha-java-basico/java-anatomia-classes/src/edu/gabryel/aplication/MinhaClasse.java
@@ -0,0 +1,18 @@
+package edu.gabryel.aplication;
+
+public class MinhaClasse {
+
+public static void main (String [] args) {
+    String primeiroNome = "Gabryel";
+    String segundoNome = "Oliveira";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
+    
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+
+}
---
+++ APRENDENDO-A-SINTAXE-JAVA/trilha-java-basico/java-anatomia-classes/src/edu/gabryel/testes/BoletimEstudantil.java
@@ -0,0 +1,5 @@
+package edu.gabryel.testes;
+
+public static void main(String[] args) {
+    
+} 
